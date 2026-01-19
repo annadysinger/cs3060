827d373048c8ac633b8d439bb57e009d1c7fc0f9
annadysinger <97917461+annadysinger@users.noreply.github.com>
2026-01-19 13:35:20 -0500

    Slow down simulation and print loop variable

--- simulate.py
+++ simulate.py
@@ -1,9 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
-
+#pauses the program for 1/60th of a second 
+#PRINT ITERATION
 for i in range(1000):
 	p.stepSimulation()
+	time.sleep(1/60)
+	print(i)
 
 p.disconnect()
 
