02b47b8b3147a47e71711fe986c20a5ff23b0f22
annadysinger <97917461+annadysinger@users.noreply.github.com>
2026-01-27 16:02:06 -0500

    Load and simulate one link

--- simulate.py
+++ simulate.py
@@ -1,7 +1,10 @@
+
 import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.loadSDF("box.sdf")
 #pauses the program for 1/60th of a second 
 #PRINT ITERATION
 for i in range(1000):
