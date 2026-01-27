b6711a8b0b1633d47cf60007bee2f34116db9c0b
annadysinger <97917461+annadysinger@users.noreply.github.com>
2026-01-27 16:09:34 -0500

    Add gravity to simulation

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import time
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.setGravity(0,0,-9.8)
+
 p.loadSDF("box.sdf")
 #pauses the program for 1/60th of a second 
 #PRINT ITERATION
