aed3a3e4b9376553569b0e55e30dde0e724f2232
annadysinger <97917461+annadysinger@users.noreply.github.com>
2026-01-27 16:14:23 -0500

    Add floor plane and collision detection

--- simulate.py
+++ simulate.py
@@ -1,10 +1,13 @@
 
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-p.setGravity(0,0,-9.8)
+p.setGravity(0,0,-9.8, physicsClient)
+planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 #pauses the program for 1/60th of a second 
