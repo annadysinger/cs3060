f77de1b2ba5cd88bb96b331c31b77ec58d74c665
annadysinger <97917461+annadysinger@users.noreply.github.com>
2026-01-29 13:11:07 -0500

    Built multi-link robots and fixed joint placement

--- generate.py
+++ generate.py
@@ -1,19 +1,111 @@
 import pyrosim.pyrosim as pyrosim
 
 # Start SDF file
-pyrosim.Start_SDF("world.sdf")  # minimal world
-
+# Step 1: Create the world
 # One cube at origin
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = height / 2   # bottom sits on floor
-
-pyrosim.Send_Cube(name="Block", pos=[x, y, z], size=[length, width, height])
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0,-3,0.5], size=[1,1,1])
 # End SDF file
-pyrosim.End()
+    pyrosim.End()
+# Step 2: Create the robot
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    
+# Parent link: Torso
+    pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5], size=[1,1,1])
+
+    # One cube as the robot's torso
+    # Joint connecting Torso to Leg
+    pyrosim.Send_Joint(
+        name="Link0_Link1",
+        parent="Link0",
+        child="Link1",
+        type="revolute",
+        position=[0,0,1.0]   # top of Torso / bottom of Leg
+    )
+
+    # Child link: Leg
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5], size=[1,1,1])
+    
+    # second joint
+    pyrosim.Send_Joint(
+        name="Link1_Link2",
+        parent="Link1",
+        child="Link2",
+        type="revolute",
+        position=[0, 0, 1.0]
+    )
+ 
+
+    # Link2 (relative to Link1_Link2)
+    pyrosim.Send_Cube(
+        name="Link2",
+        pos=[0, 0, 0.5],
+        size=[1, 1, 1]
+    )
 
 
+    # Link2 -> Link4 (sideways)
+    pyrosim.Send_Joint(
+        name="Link2_Link4",
+        parent="Link2",
+        child="Link4",
+        type="revolute",
+        position=[0, 0.5, 0.5]
+    )
 
+    pyrosim.Send_Cube(
+        name="Link4",
+        pos=[0, 0.5, 0],
+        size=[1, 1, 1]
+    )
+
+    # Link4 -> Link5 (sideways)
+    pyrosim.Send_Joint(
+        name="Link4_Link5",
+        parent="Link4",
+        child="Link5",
+        type="revolute",
+        position=[0, 1.0, 0]
+    )
+
+    pyrosim.Send_Cube(
+        name="Link5",
+        pos=[0, 0.5, 0],
+        size=[1, 1, 1]
+    )
+	
+    # Joint 5 -> 6 (DOWN)
+    pyrosim.Send_Joint(
+        name="Link5_Link6",
+        parent="Link5",
+        child="Link6",
+        type="revolute",
+        position=[0, 0.5, -0.5]
+    )
+
+# Link6 (relative to Link5_Link6)
+    pyrosim.Send_Cube(
+        name="Link6",
+        pos=[0, 0, -0.5],
+        size=[1, 1, 1]
+    )
+
+    # Link6 -> Link7 (down)
+    pyrosim.Send_Joint(
+        name="Link6_Link7",
+        parent="Link6",
+        child="Link7",
+        type="revolute",
+        position=[0, 0, -0.5]
+    )
+
+    pyrosim.Send_Cube(
+        name="Link7",
+        pos=[0, 0, -1.0],
+        size=[1, 1, 1]
+    )
+    pyrosim.End()
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -4,13 +4,24 @@ import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setGravity(0,0,-9.8, physicsClient)
+
 planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("world.sdf")
+# Robot
+robotId = p.loadURDF("body.urdf")  # the robot link
+
+p.resetDebugVisualizerCamera(
+    cameraDistance=7,
+    cameraYaw=45,
+    cameraPitch=-30,
+    cameraTargetPosition=[0, 0, 1])  # roughly center on the robot
 #pauses the program for 1/60th of a second 
+
 #PRINT ITERATION
 for i in range(1000):
 	p.stepSimulation()
